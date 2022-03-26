78dbac1b7d45de3c2b6607ddf632d5c86c178fdc
Grisha Angelov <grisha.angelov89@gmail.com>
2022-03-26 15:04:15 +0200

    Draw 5x6 Box grid

--- src/components/Wordy/Wordy.js
+++ src/components/Wordy/Wordy.js
@@ -1,9 +1,35 @@
+import { useState } from 'react'
+
+import Box from './Box'
+
 const Wordy = () => {
+    const [words, setWords] = useState([
+        ['', '', '', '', ''],
+        ['', '', '', '', ''],
+        ['', '', '', '', ''],
+        ['', '', '', '', ''],
+        ['', '', '', '', ''],
+        ['', '', '', '', '']
+    ])
 
     return (
         <div className="row mt-3">
             <div className="col-md-12">
                 <h3 className="alert-success rounded">Wordy</h3>
+
+                <div className="mt-3">
+                    {
+                        words.map((word, i) => (
+                            <div key={i} className="d-flex justify-content-center">
+                                {
+                                    word.map((v, j) => (
+                                        <Box key={`${i}_${j}`} value={v} />
+                                    ))
+                                }
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
