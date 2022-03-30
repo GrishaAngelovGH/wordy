febcabb3c847560a28af2230c332eb18a845905a
Grisha Angelov <grisha.angelov89@gmail.com>
2022-03-30 17:06:28 +0300

    Change text color in Box based on background color

--- src/components/Wordy/Box/Box.js
+++ src/components/Wordy/Box/Box.js
@@ -1,9 +1,13 @@
 import './Box.css'
 
-const Box = ({ value, color }) => (
-    <div className={`d-flex justify-content-center align-items-center border border-2 fw-bold rounded box ${color}`}>
-        {value}
-    </div>
-)
+const Box = ({ value, color }) => {
+    const textColor = color === 'bg-white' ? 'text-dark' : 'text-white'
+
+    return (
+        <div className={`d-flex justify-content-center align-items-center border border-2 fw-bold rounded box ${color} ${textColor}`}>
+            {value}
+        </div>
+    )
+}
 
 export default Box
