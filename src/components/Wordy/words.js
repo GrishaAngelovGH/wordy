9ecac9db25fb3df9d0784323c23278ecfa17ff79
Grisha Angelov <grisha.angelov89@gmail.com>
2022-03-30 11:13:14 +0300

    Add more target words

--- src/components/Wordy/words.js
+++ src/components/Wordy/words.js
@@ -1,10 +1,34 @@
-export default [
-    'mount', 'power', 'north', 'toner', 'month', 'perky',
-    'think', 'kings', 'drink', 'marks', 'porky', 'could',
-    'sport', 'mover', 'doner', 'hover', 'wrung', 'bread',
-    'treat', 'pride', 'trone', 'grand', 'great', 'light',
-    'pizza', 'titan', 'dirts', 'lists', 'limit', 'those',
-    'where', 'there', 'moist', 'pears', 'vivid', 'beard',
-    'coned', 'phone', 'toned', 'wined', 'mixed', 'crowd',
-    'proud', 'spear', 'drone', 'grape', 'thing'
+const words = [
+    'mount', 'power', 'north', 'month', 'think',
+    'kings', 'drink', 'marks', 'sport', 'bread',
+    'drive', 'pride', 'grand', 'great', 'light',
+    'moist', 'pears', 'found', 'beard', 'phone',
+    'mixed', 'crowd', 'proud', 'spear', 'drone',
+    'grape', 'thing', 'model', 'fresh', 'first',
+    'stand', 'print', 'drain', 'panel', 'round',
+    'basic', 'spicy', 'thumb', 'world', 'smart',
+    'radio', 'store', 'solve', 'speak', 'vital',
+    'rhino', 'trace', 'peach', 'solar', 'point',
+    'sugar', 'frame', 'pause', 'shake', 'other',
+    'watch', 'today', 'focus', 'movie', 'royal',
+    'flair', 'plant', 'olive', 'story', 'train',
+    'badge', 'metal', 'gamer', 'money', 'album',
+    'depth', 'float', 'input', 'field', 'voice',
+    'night', 'prize', 'enjoy', 'valid', 'equal',
+    'dream', 'baker', 'count', 'study', 'lunch',
+    'quick', 'honey', 'price', 'grace', 'cream',
+    'reply', 'heart', 'diner', 'smile', 'build',
+    'write', 'slide', 'union', 'lucky', 'house',
+    'place', 'index', 'juicy', 'stage', 'clean',
+    'ridge', 'vowel', 'thank', 'track', 'range',
+    'brain', 'candy', 'layer', 'fluid', 'score',
+    'sound', 'ocean', 'solid', 'relax', 'laugh',
+    'earth', 'brave', 'guest', 'child', 'share',
+    'pearl', 'large', 'plane', 'crown', 'beach',
+    'trade', 'roast', 'party', 'touch', 'ready',
+    'bonus', 'young', 'shape', 'short', 'space',
+    'logic', 'towel', 'value', 'route', 'sharp',
+    'pixel', 'audio', 'topic', 'tower', 'music'
 ]
+
+export default words
